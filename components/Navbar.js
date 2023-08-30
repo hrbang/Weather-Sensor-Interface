2f3f4849e42b2a1cbbf84637c8ed9781368c7ad9
Jonas Bang <jonasbang@hotmail.dk>
2023-08-30 21:27:51 +0200

    Added navigation bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,49 @@
-import React from 'react'
-import { styled } from "styled-components"
+import { styled } from "styled-components";
+
 
 const Nav = styled.nav`
-    margin-top: 32px;
-    background-color: #252524;
-    height: 80px;
+    position: fixed;
+    bottom: 50px;
+    left: 50%;
+    transform: translateX(-50%);
+`
+
+const NavInner = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0 32px;
-    z-index: 100;
-    border-radius: 12px;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    box-shadow: 0px 2px 75px 0px rgba(37, 37, 37, 0.1);
+    padding: 16px;
+    border-radius: 100vw;
 `
 
-const Title = styled.h1`
-    color: #fff;
-    font-size: 24px;
+const NavItem = styled.button`
+    font-size: 18px;
     font-weight: 700;
-    letter-spacing: 1.4px;
+    color: #252525;
+    text-decoration: none;
+    background-color: ${props => props.isActive ? '#25f595' : 'transparent'};
+    padding: 10px 20px;
+    border-radius: 100vw;
+    border: none;
+    outline: none;
+    transition: background-color 220ms ease-in-out, color 220ms ease-in-out;
 `
 
-const Navbar = () => {
+const Navbar = ({ isActive, onClickHandler }) => {
     return (
         <Nav>
-            <div>
-                <Title>Weather Interface</Title>
-            </div>
+            <NavInner>
+                <NavItem isActive={isActive === "charts"} onClick={() => onClickHandler("charts")}>
+                    Data charts
+                </NavItem>
+                <NavItem isActive={isActive === "documentation"} onClick={() => onClickHandler("documentation")}>
+                    Documentation
+                </NavItem>
+            </NavInner>
         </Nav>
     )
 }
